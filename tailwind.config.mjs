6c82a6848391f2f79875c825cbcade035d79a1bf
Miguel <gallegosmiguel2000@gmail.com>
2025-02-19 16:49:14 -0400

    feat: new breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,20 @@
-const {heroui} = require("@heroui/theme");
+const { heroui } = require("@heroui/theme");
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",],
-	theme: {
-		extend: {
-			colors:{
-				'main': '#001d4a'
-			},
-			breakpoints: {
-				screens: {
-					'2md': '859px',
-				},
-			},
-		},
-	},
-	darkMode: "class",
-	plugins: [require('daisyui'), heroui()],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        main: "#001d4a",
+      },
+      screens: {
+        "2md": "859px",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [require("daisyui"), heroui()],
+};
