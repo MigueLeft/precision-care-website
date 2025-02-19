13b329b7b09af49227f50908387460c585ca701c
Miguel <gallegosmiguel2000@gmail.com>
2025-02-19 16:57:42 -0400

    fix: import vercelAdapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import vercelAdapter from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercelAdapter(),
   integrations: [tailwind(), react()],
 });
