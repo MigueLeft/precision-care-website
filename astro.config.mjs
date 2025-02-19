91e194cb4776eadeb876be7f370e991a9478f6b2
Miguel <gallegosmiguel2000@gmail.com>
2025-02-19 19:08:22 -0400

    fix: lockfile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
 import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercelAdapter(),
-  integrations: [react(), tailwindcss()],
+  integrations: [tailwind(), react()],
 });
